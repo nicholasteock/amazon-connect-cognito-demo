ef8215687db1aa941c18feebd8652e863c557fe9
Nicholas Teo <nicholasteock@gmail.com>
2023-01-30 10:58:57 +0800

    update amplify auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,14 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "lc60demobd596738": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "CreatedSNSRole": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
