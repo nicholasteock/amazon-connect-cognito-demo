3a59085ab7a6b72ad75845f11692ae95d7b03fc4
Nicholas Teo <nicholasteock@gmail.com>
2023-01-30 10:22:22 +0800

    initial commits

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
 import '@aws-amplify/ui-react/styles.css';
 
-function App(signOut, user) {
+Amplify.configure(awsconfig);
+
+function App({signOut, user}) {
   return (
     <div className="App">
-      <div style={styles.container}>
         <Heading level={1}>Hello {user.username}</Heading>
-        <Button onClick={signOut}>Sign out</Button>
         <h2>Amplify Demo</h2>
-      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
